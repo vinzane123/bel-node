912cc28d998e54267e6ca0dfdba1fb014cb550a9
Rajat Kesharwani <rajat.kesharwani@oodlestechnologies.com>
2018-08-02 17:32:35 +0530

    updated constant.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -20,7 +20,7 @@ module.exports = {
   maxTxsPerBlock: 500,
   initialAmount: 20000000000,
   expDateOfKYC: 2, // default exp date of kyc in years
-  defaultCurrency: 'BEL',
+  defaultCurrency: 'BEL', // default currency symbole for Belrium
   fees: {
     send: 0.001,
     secondsignature: 0.001,
